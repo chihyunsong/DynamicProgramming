8aa30905afd0d98ff085369b2cba3f9f8669a3d3
Justin <newcoke2008@gmail.com>
2019-03-05 18:32:02 -0500

    Adds tripleStep Solution

---
+++ tripleStep.py
@@ -0,0 +1,23 @@
+def tripleStep(n):
+    if n == 0:
+         return 0
+    if n == 1:
+         return 1
+    if n == 2:
+         return 2
+    if n == 3:
+        return 4
+    # base case covered.
+    # Initialize
+    result = [0] * (n+1)
+    result[0] = 0
+    result[1] = 1
+    result[2] = 2
+    result[3] = 4
+
+    for i in range(4, n+1):
+        result[i] = result[i-1] + result[i-2] + result[i-3]
+    return result[i]
+
+
+print (tripleStep(5))
